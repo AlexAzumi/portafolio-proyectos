34b61d3ce0a6fd9dc74f3bc5a8f075444c65752b
Alejandro Suárez <alejandro-hdez115@outlook.com>
2019-05-06 23:34:46 -0500

    Implementar AOS
    - Se implementó la librería AOS para animaciones
    - El spacing del componente home fue reducido en teléfonos
    - La barra de navegación tiene un color sólido en dispositivos móviles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+// Dependencias
+import { Component, OnInit } from '@angular/core';
+import AOS from 'AOS';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'azumi-web';
+export class AppComponent implements OnInit {
+  /*
+   * OnInit
+   */
+  ngOnInit() {
+    AOS.init();
+  }
 }

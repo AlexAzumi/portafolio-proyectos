e3ae582a3fa5d60e48deb0bdd77114a74a2871e1
Alejandro Suárez <alejandro-hdez115@outlook.com>
2019-05-29 12:34:14 -0500

    Revert "Revert "Títulos más pequeños""
    This reverts commit e1b6625674bf724dafd7a842c437a81472791492.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  // Remover logs
+  if (window) {
+    window.console.log = function() { }
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)

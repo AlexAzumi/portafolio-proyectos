08a8e4980f22aa63fd1de986cd5b35b9bbfa770b
Alejandro Suárez <alejandro-hdez115@outlook.com>
2020-06-18 11:15:58 -0500

    Mudar proyecto a Next.js

--- src/app/models/project.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Project {
-    title: String;
-    description: String;
-    date: String;
-    img: String;
-    url: String;
-}
--- src/app/models/repository.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Repository {
-  name: String;
-  full_name: String;
-  html_url: String;
-  description: String;
-  fork: Boolean;
-  homepage: String;
-}

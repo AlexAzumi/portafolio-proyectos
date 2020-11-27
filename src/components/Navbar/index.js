2c1f3241fa7190b69faea4ba570e9c72f7e6e9ce
Alejandro Suárez <alejandro-hdez115@outlook.com>
2020-11-27 17:04:29 -0600

    Remove address bar updates

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -26,8 +26,6 @@ const Navbar = () => {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
       // Collapse navbar
       setCollapsed(true);
-      // Update address bar
-      window.history.pushState('', "Azumi's vault", `/#${elementID}`);
     }
   };
 
